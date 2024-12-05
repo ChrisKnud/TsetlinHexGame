e0fb34437c1e986534e31802fe8df67e90b5af99
Christoffer L. Knudsen <clangs.dev@gmail.com>
2024-12-05 18:09:43 +0100

    Added function to remove random pieces from the board

--- generate_training_data/self_generated.py
+++ generate_training_data/self_generated.py
@@ -3,7 +3,7 @@ import math
 import random
 from datetime import datetime
 
-from format import board_as_string
+#from format import board_as_string
 from generate_training_data.util.check_winner import check_winner
 
 
@@ -28,19 +28,61 @@ class HexBoard:
             "board": self.board,
         }
 
-    def _populate(self):
-        print("board: " + str(self.board))
-        for i in range(len(self.board)):
+
+    """
+    Remove a certain number of players from the board.
+    This returns an unfinished board but with a known winner.
+    """
+    def remove_players(self, count):
+        if self.winner is None:
+            raise ValueError("Board has no winner. Please generate a full board first.")
+
+        player = random.randint(0, 1)
+
+        for i in range(count):
             index = random.randint(0, len(self.board) - 1)
 
-            while self.board[index - 1] != self.symbols[2]:
-                index = random.randint(0, len(self.board) - 1)
-                print("Index " + str(index))
+            # Make sure the removed piece is not empty
+            if player == 0:
+                # Make sure the piece to remove is symbol 2
+                while self.board[index] != self.symbols[0]:
+                    index = random.randint(0, len(self.board) - 1)
 
-            if self.count % 2 == 0:
-                self.board[index-1] = self.symbols[0]
+                self.board[index] = self.symbols[2]
+                print(f"player {player} on index {index} set to {self.symbols[2]}")
+                player = 1
+                continue
+
+
+            if player == 1:
+                # Make sure the piece to remove is symbol 1
+                while self.board[index] != self.symbols[1]:
+                    index = random.randint(0, len(self.board) - 1)
+
+                self.board[index] = self.symbols[2]
+                print(f"player {player} on index {index} set to {self.symbols[2]}")
+                player = 0
+                continue
+
+
+    def _populate(self):
+        populated_pos = []
+        finished = False
+
+        while not finished:
+            index = random.randint(0, len(self.board) - 1)
+
+            while self.board[index] in populated_pos:
+                index = random.randint(0, len(self.board) - 1)
+
+            populated_pos.append(index-1)
+
+            # count != len(board) to mitigate a player
+            # having two more pieces when the game is finished
+            if self.count % 2 == 0 and self.count != len(self.board) - 1:
+                self.board[index] = self.symbols[0]
             else:
-                self.board[index - 1] = self.symbols[1]
+                self.board[index] = self.symbols[1]
 
             self.count += 1
 
@@ -48,21 +90,40 @@ class HexBoard:
 
             if check_winner(self.board, self.size) is not None:
                 self.winner = winner
-                return
-
-        print("board " + str(self.board))
+                finished = True
 
 
-def generate_hex_games(size, count, split=False):
+def board_as_string(board):
+    board_width = int(math.sqrt(len(board)))
+
+    board_str = ""
+    for i in range(len(board)):
+        if board[i] == 'E':
+            board_str += '. '
+        else:
+            board_str += str(board[i]) + " "
+        if i != 0 and i % board_width == board_width - 1:
+            board_str += "\n"
+
+    return board_str
+
+
+# Moves left: Number of moves before the game is finished
+def generate_hex_games(size, count, moves_left=0, split=False):
     hexboard = HexBoard(size, ["B", "W", "."])
 
     boards = []
     for i in range(count):
         hexboard.make_board()
+
+        if moves_left > 0:
+            hexboard.remove_players(moves_left)
+
         print(board_as_string(hexboard.board))
         print(hexboard.board_as_json())
-        boards.append(hexboard.board_as_json())
 
+        boards.append(hexboard.board_as_json())
+        print("Created boards: " + str(i))
     dt = datetime.now().strftime("%Y-%m-%d-%H%M%S")
 
     path = f"D:\\Software_Development\\Artificial_Intellegence\\TsetlingMachine\\TsetlinHexGame\\data\\self"
@@ -83,4 +144,4 @@ def generate_hex_games(size, count, split=False):
             f.write(json.dumps({"result": boards}, indent=4))
 
 
-generate_hex_games(7, 8, True)
+generate_hex_games(7, 1, moves_left=2, split=False)
